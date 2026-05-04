00e9c461cc33fd5ac003c58cd3f3adb56936c326
Setatian Cortes <samuelandresromerobueno27@gmail.com>
2026-05-04 09:29:03 -0500

    Creación Cors

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
+
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
+
 
 morgan.token('body', (request) => {
     if (request.method === 'POST') {
